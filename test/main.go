34a80d1e5a9e3c7f7c8e61f8bd750b521d3dd1f8
shaneda <shaneda@tencent.com>
2023-08-11 17:14:37 +0800

    fixed import cycle

---
+++ test/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+)
+
+func main() {
+	arr := []int64{1, 2, 3, 4, 5, 6, 5, 4}
+	// 将 int64 数组编码为字节数组
+	buf := new(bytes.Buffer)
+	for _, v := range arr {
+		binary.Write(buf, binary.LittleEndian, v)
+	}
+	encoded1 := buf.Bytes()
+
+	// 将字节数组解码为 int64 数组
+	decoded1 := make([]int64, len(arr))
+	buf = bytes.NewBuffer(encoded1)
+	for i := range arr {
+		fmt.Println(i)
+		binary.Read(buf, binary.LittleEndian, &decoded1[i])
+	}
+
+	fmt.Println(encoded1)
+	fmt.Println(decoded1)
+
+	encoded, _ := encode(arr)
+	fmt.Println(encoded)
+	decoded, _ := decode(encoded)
+	fmt.Println(decoded)
+}
+
+func encode(int64Slice []int64) ([]byte, error) {
+	// 将 []int64 切片转换为 []byte
+	buf := new(bytes.Buffer)
+	for _, v := range int64Slice {
+		binary.Write(buf, binary.LittleEndian, v)
+	}
+	encoded := buf.Bytes()
+	return encoded, nil
+}
+
+func decode(byteSlice []byte) ([]int64, error) {
+	la := len(byteSlice) / 8
+	// 将 []byte 切片转换为 []int64
+	decoded := make([]int64, la)
+	buf := bytes.NewBuffer(byteSlice)
+	for i := 0; i < la; i++ {
+		binary.Read(buf, binary.LittleEndian, &decoded[i])
+	}
+	return decoded, nil
+}
